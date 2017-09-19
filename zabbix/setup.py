06a7cd7d2fcbd9ad74c0ee3577b586156c3d8ab6
Aleksandr Balezin <gescheit@yandex-team.ru>
2017-09-19 11:16:12 +0300

    release 0.5

--- zabbix/setup.py
+++ zabbix/setup.py
@@ -15,7 +15,7 @@ def read(fname):
 setup(
     name='zabbix-api',
     url='https://github.com/gescheit/scripts',
-    version='0.3',
+    version='0.5',
     license='GNU LGPL 2.1',
     author='Aleksandr Balezin',
     author_email='gescheit12@gmail.com',
