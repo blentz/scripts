45d20e43f5a015a04e39058dc2c67c7c097b3ae8
Aleksandr Balezin <gescheit@yandex-team.ru>
2017-09-19 12:29:45 +0300

    fix pypi doc

--- zabbix/setup.py
+++ zabbix/setup.py
@@ -8,19 +8,26 @@ import os
 from setuptools import setup, find_packages, findall
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read_descr(fname):
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    try:
+        import pypandoc
+        long_description = pypandoc.convert(filepath, 'rst')
+    except(IOError, ImportError):
+        long_description = open(filepath).read()
+
+    return long_description
 
 
 setup(
     name='zabbix-api',
     url='https://github.com/gescheit/scripts',
-    version='0.5',
+    version='0.5.1',
     license='GNU LGPL 2.1',
     author='Aleksandr Balezin',
     author_email='gescheit12@gmail.com',
     description='Zabbix API',
-    long_description=read('README.md'),
+    long_description=read_descr('README.md'),
     py_modules=['zabbix_api'],
     include_package_data=True,
     zip_safe=False,
